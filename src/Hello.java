767783e0df3018188f7befc159f2086bf3a4e5ae
mrgoggl3s <razvan.jijau@gmail.com>
2019-04-15 22:58:50 +0300

    my first project

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        int primulnr = (15 + 3) + (3 * 4);
+        int al2leanr = 1;
+        int al3leanr = primulnr + 3;
+
+
+        int sumalor = primulnr + al2leanr + al3leanr;
+
+
+        int ultima = 100101 - sumalor;
+
+        System.out.println(ultima);
+
+
+
+    }
+
+}
